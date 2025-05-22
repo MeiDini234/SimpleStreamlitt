4ad74ce285445a938c20d22fd9c57637d763a5c5
MeiDini234 <meilanidini717@students.unnes.ac.id>
2025-05-22 09:16:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="Prediction", layout="centered")
 st.title("Hello, World!")
 st.write("Ini aplikasi Streamlit pertamaku 🎈")
 
-st.set_page_config(page_title="Prediction", layout="centered")
+
